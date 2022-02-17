efc5bd8a9f67a9fb382ce9bf5c258c1982875eab
ReaLdr <dilemajire@gmail.com>
2022-02-16 19:48:11 -0600

    Login y Auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
@@ -17,7 +19,9 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [
     LoginComponent,
